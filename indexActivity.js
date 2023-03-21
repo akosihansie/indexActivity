2f66235ee185b389f36e0431d5c0398d056ebee5
hansie <lhg0694@dlsud.edu.ph>
2023-03-21 09:14:53 +0800

    Add activity code

---
+++ indexActivity.js
@@ -0,0 +1,21 @@
+console.log('Hello, Welcome to De La Salle University!');
+
+let firstName = 'Hans';
+let lastName = 'Laureles';
+let age = 20;
+let hobbies = ['Listening to Music', 'Playing Video Games', 'Watching Anime', 'Reading Manga', 'Learning Video Editing'];
+let homeAddress = {
+	houseNumber: '139',
+	street: 'Purok',
+	subdivision: 'Buho',
+	barangay: 'Buho',
+	city: 'Amadeo',
+	province: 'Cavite',
+	zipCode: 4119
+};
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(hobbies);
+console.log(homeAddress);
